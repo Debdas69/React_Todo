944aa89688d14091426719ac6a204edfa212a9f1
Debdas69 <deb05838@gmail.com>
2025-03-09 23:42:01 +0530

    Build a Complete Todo App from Scratch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './projects/Todo/Todo'
+
+const App = () => {
+  return (
+    <>
+    <section>
+      <Todo/>
+    </section>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/projects/Todo/Todo.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import {MdCheck,MdDeleteForever} from "react-icons/md"
+
+const Todo = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [task, setTask] = useState([]);
+
+  const handleInputChange = (value) => {
+      setInputValue(value)
+    console.log(value)
+  }
+
+  const handleFormSubmit = (event)=> {
+    event.preventDefault();
+
+    if(!inputValue) return;
+
+    if(task.includes(inputValue)) {
+      setInputValue("")
+      return
+    };
+
+    setTask((prevTask) => [...prevTask, inputValue])
+
+    setInputValue("");
+  }
+  return (
+    <>
+
+  <section className='todo-container  bg-amber-50 m-5'>
+    <header>
+      <h1 className='font-bold text-2xl'>Todo List</h1>
+    </header>
+    <section className='form'>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <input type="text" onChange={(e)=>handleInputChange(e.target.value)} className='todo-input border-2 rounded-2xl px-1 py-1' autoComplete='off' value={inputValue}/>
+        </div>
+        <div>
+          <button type='submit' className='todo-btn bg-blue-400 hover:bg-yellow-400 text-white rounded-md p-2 m-2'>Add Task</button>
+        </div>
+      </form>
+    </section>
+      <section>
+        <ul>
+          {
+            task.map((curTask,index) => {
+              return(
+                <li key={index} className='text-2xl font-bold p-2 flex items-center justify-between w-[200px] h-[60px] bg-amber-500 rounded-md mb-2'>
+                  <span className=''>{curTask}</span>
+                  <button>
+                    <MdCheck className=' bg-green-400 text-3xl hover:bg-green-800 text-white rounded-md '/>
+                  </button>
+                  <button>
+                    <MdDeleteForever className=' bg-red-400 text-3xl hover:bg-red-700 text-white rounded-md'/>
+                  </button>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+
+  </section>
+
+    </>
+  )
+}
+
+export default Todo
